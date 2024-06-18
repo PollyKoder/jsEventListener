b1742ea60fb119ca3c90fdeb5ef30259479a4cf3
Polly <riviera.koder@gmail.com>
2024-06-18 13:26:23 +0200

    keyCodesFoundAndSortedImg

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+wasd coeds:
+
+w = 87
+a = 65 (e.keyCode65)
+s = 83
+d = 68
+
+space = 32 (e.keyCode32)
--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const page = document.getElementById("#bodyId");
-const grief = document.getElementById("#stages");
+const page = document.getElementById("bodyId");
+const grief = document.getElementById("stages");
 
 
 function denialAnger() {
-    
+    grief.addEventListener("click", function() {alert("Use your keyboard please")});
+}
+
+denialAnger();
+console.log(grief);
+
+function anger() {
+    grief.addEventListener("keydown", w)
 }
