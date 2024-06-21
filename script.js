a06d17efdb7700aff9a2007e7c96965155ee0891
PollyKoder <riviera.koder@gmail.com>
2024-06-21 18:03:50 +0200

    gitUpdateToHomePc_minorCssChanges

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const page = document.getElementById("#bodyId");
-const grief = document.getElementById("#stages");
+const page = document.getElementById("bodyId");
+const grief = document.getElementById("stages");
 
 
 function denialAnger() {
-    
+    grief.addEventListener("click", function() {alert("Use your keyboard please")});
+}
+
+denialAnger();
+console.log(grief);
+
+
+let whatStage = 0;
+function anger() {
+    grief.addEventListener("keydown", )
 }
